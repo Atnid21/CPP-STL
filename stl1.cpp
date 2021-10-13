31eda8b8db4384e19ca996ee28643c29e40a121e
Atharva Nidhonkar <81277887+Atnid21@users.noreply.github.com>
2021-10-13 08:58:25 +0530

    Add files via upload

---
+++ stl1.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+// 	array<int,5> arr={1,3,4,5,6};
+
+// 	 // begin() and end()
+
+// 	for(auto it=arr.begin(); it!=arr.end();it++){
+// 		cout<< *it<<" ";
+// 	}
+// 	cout<<endl;
+
+// 	// rbegin() and rend()
+// for(auto it=arr.rbegin(); it<arr.rend();it++){
+// 		cout<< *it<<" ";
+// 	}
+
+// // 	cout<<endl;
+
+// // 	for(auto it:arr){  // for each loop
+// // 		cout<<it<<" ";
+// // 	}
+
+
+// Vector
+
+	// vector<int>arr;
+	// cout<<arr.size()<<endl; // 0
+
+	// arr.push_back(3); // insert element
+	// arr.push_back(4);
+
+	// cout<<arr.size()<<endl; //2
+
+	// arr.pop_back(); // removes last element  
+
+	// for(int i=0;i<arr.size();++i){
+	// 	cout<<arr[i]<<" ";
+	// }
+
+	// vec.clear();
+
+	// for(int i=0;i<arr.size();++i){
+	// 	cout<<arr[i]<<" ";
+	// }
+
+	// // DECLARATION
+	// vector<int> vec(4,0);
+	// vector<int> vec1(4,10);
+
+	// // vector<int> vec2(vec1.begin(),vec1.end());
+
+	// std::vector<int> vec2(vec);
+	// cout<<vec2[2]<<endl;
+
+	// vector<vector<int>>vec; //-->2d vector
+	
+	// vector<int>vec1;
+	// vec1.emplace_back(1);
+	// vec1.emplace_back(2);
+
+	// vector<int>vec2;
+	// vec1.emplace_back(10);
+	// vec1.emplace_back(20);
+
+
+	// for(int i=0;i<vec2.size();++i)
+	// 	cout<<vec2[i]<<" ";
+
+	// 10 x 20
+	// vector<vector<int>> v(10,vector<int>v1(20));
+	
+	// vector<vector<vector<int>>> v(10,vector<int>v1(20,vector<int>v2(30)))
+
+	
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
